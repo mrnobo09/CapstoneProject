ab247914dc196f966da32314af18f944ada16c32
mrnobo09 <137929660+mrnobo09@users.noreply.github.com>
2023-07-27 16:21:59 +0500

    Lil Lemon Site

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import './App.css';
-import {Footer,Header,Main,Nav} from "./Components";
+import React from "react";
+import Home from "./routes/Home";
+import Reservations from "./routes/Reservations";
+
+import {Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Header/>
-      <Nav/>
-      <Main/>
-      <Footer/>
-
+      <Routes>
+        <Route path = "/" element = {<Home />}/>
+        <Route path = "/reservations" element = {<Reservations />}/>
+      </Routes>
     </>
   );
 }
